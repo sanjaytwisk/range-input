2205c7e53b81d77a179ff1bca5ca5763bd6a1be3
Sanjay Twisk <sanjay@acolorbright.com>
2020-02-11 16:29:29 +0100

    feat: drag handles to update the value

--- src/RangeInput.stories.tsx
+++ src/RangeInput.stories.tsx
@@ -1,19 +1,43 @@
 import * as React from 'react'
+import { withA11y } from '@storybook/addon-a11y'
+import { action } from '@storybook/addon-actions'
 import { RangeInput } from './RangeInput'
 
 export default {
   title: 'Range Input',
   component: RangeInput,
+  decorators: [withA11y],
 }
 
 export const primary = () => (
-  <RangeInput name="range" step={1} min={0} max={10} value={[0, 3]} />
+  <RangeInput
+    name="range"
+    step={1}
+    min={0}
+    max={10}
+    value={[0, 3]}
+    onChange={action('change')}
+  />
 )
 
 export const secondary = () => (
-  <RangeInput name="range" step={4} min={0} max={16} value={[0, 4]} />
+  <RangeInput
+    name="range"
+    step={4}
+    min={0}
+    max={16}
+    value={[0, 4]}
+    onChange={action('change')}
+  />
 )
 
 export const tertiary = () => (
-  <RangeInput name="range" step={0.1} min={0} max={10} value={[0, 2]} />
+  <RangeInput
+    name="range"
+    step={0.1}
+    min={0}
+    max={10}
+    value={[0, 2]}
+    onChange={action('change')}
+  />
 )
--- src/RangeInput.tsx
+++ src/RangeInput.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-const { useState } = React
+const { useState, useEffect, useRef } = React
 
 import './range-input.css'
 
@@ -9,7 +9,7 @@ export interface RangeInputProps {
   max: number
   step: number
   value?: number[]
-  onChange?: (evt: React.ChangeEvent<HTMLInputElement>) => void
+  onChange?: (change: { name: string; value: [number, number] }) => void
 }
 
 export const RangeInput: React.SFC<RangeInputProps> = ({
@@ -20,24 +20,88 @@ export const RangeInput: React.SFC<RangeInputProps> = ({
   step,
   onChange,
 }) => {
+  const nameMin = `${name}[min]`
+  const nameMax = `${name}[max]`
   const [initMin, initMax] = value || [min, max]
-  const [minValue, setMinValue] = useState(initMin)
-  const [maxValue, setMaxValue] = useState(initMax)
+  const [localValue, setLocalValue] = useState({
+    [nameMin]: initMin,
+    [nameMax]: initMax,
+  })
+  const isMouseDown = useRef(false)
+  const rangeElement = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (onChange) {
+      onChange({
+        name,
+        value: [localValue[nameMin], localValue[nameMax]],
+      })
+    }
+  }, [localValue])
+
+  const minValue = localValue[nameMin]
+  const maxValue = localValue[nameMax]
   const onChangeMin = (evt: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseFloat(evt.target.value)
     if (value >= maxValue) return
-    setMinValue(value)
+    setLocalValue({ ...localValue, [nameMin]: value })
   }
+  const validateValue = (
+    thumb: typeof nameMin | typeof nameMax,
+    nextValue: number
+  ) => {
+    if (thumb === nameMin && (nextValue >= maxValue || nextValue < min)) {
+      return false
+    }
+    if (thumb === nameMax && (nextValue <= minValue || nextValue > max)) {
+      return false
+    }
+    return true
+  }
+
   const onChangeMax = (evt: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseFloat(evt.target.value)
     if (value <= minValue) return
-    setMaxValue(value)
+    setLocalValue({ ...localValue, [nameMax]: value })
   }
+
   const getPosition = (value: number) => {
     return (value / max) * 100
   }
+
+  const onMouseDown = () => {
+    isMouseDown.current = true
+  }
+  const onMouseUp = () => {
+    isMouseDown.current = false
+  }
+  const onMouseMove = (thumb: typeof nameMin | typeof nameMax) => (
+    evt: React.MouseEvent<HTMLLabelElement>
+  ) => {
+    if (isMouseDown.current && rangeElement.current) {
+      const rangeElementClient = rangeElement.current.getBoundingClientRect()
+      const dragPosition = evt.clientX - rangeElementClient.left
+      const nextValue = getValueByPosition(
+        dragPosition,
+        rangeElementClient.width
+      )
+      const currentValue = localValue[thumb]
+      if (
+        nextValue !== currentValue &&
+        Math.round(nextValue % step) === 0 &&
+        validateValue(thumb, nextValue)
+      ) {
+        setLocalValue({ ...localValue, [thumb]: nextValue })
+      }
+    }
+  }
+  const getValueByPosition = (position: number, width: number): number => {
+    const stepSizePixel = width / (max / step)
+    const roundTo = step < 1 ? 10 : 1
+    return Math.round((position / stepSizePixel) * step * roundTo) / roundTo
+  }
+
   return (
-    <div className="range">
+    <div className="range" ref={rangeElement}>
       <div className="range__track" />
       <div
         className="range__fill"
@@ -48,37 +112,54 @@ export const RangeInput: React.SFC<RangeInputProps> = ({
       />
       <input
         type="range"
-        value={minValue}
+        value={localValue[nameMin]}
         min={min}
         max={max}
         step={step.toString()}
         className="range__input"
         onChange={onChangeMin}
-        id="range-min"
+        id={nameMin}
+        name={nameMin}
       />
       <label
         className="range__label"
-        htmlFor="range-min"
-        style={{ left: `calc(${getPosition(minValue)}% - 0.5rem)` }}
-      />
+        htmlFor={nameMin}
+        style={{ left: `calc(${getPosition(localValue[nameMin])}% - 0.5rem)` }}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseLeave={onMouseUp}
+        onMouseMove={onMouseMove(nameMin)}
+        draggable={false}
+      >
+        Minimum value
+      </label>
       <input
         type="range"
-        value={maxValue}
+        value={localValue[nameMax]}
         min={min}
         max={max}
         step={step.toString()}
         className="range__input"
         onChange={onChangeMax}
-        id="range-max"
+        id={nameMax}
+        name={nameMax}
       />
       <label
         className="range__label"
-        htmlFor="range-max"
-        style={{ left: `calc(${getPosition(maxValue)}% - 0.5rem)` }}
-      />
-      {/* <output>
-        {minValue}, {maxValue}
-      </output> */}
+        htmlFor={nameMax}
+        style={{ left: `calc(${getPosition(localValue[nameMax])}% - 0.5rem)` }}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseLeave={onMouseUp}
+        onMouseMove={onMouseMove(nameMax)}
+        draggable={false}
+      >
+        Maximum value
+      </label>
+      <output className="range__output">
+        <span>{localValue[nameMin]}</span>
+        <span>{localValue[nameMax]}</span>
+      </output>
     </div>
   )
 }
